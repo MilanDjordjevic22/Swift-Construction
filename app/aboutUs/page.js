70ea75f2b736f4691159b2ccc95fa035e8b52d8d
Milan Djordjevic <milan.djordjevic@hotmail.com>
2025-03-31 12:01:17 -0600

    added navigation

--- app/aboutUs/page.js
+++ app/aboutUs/page.js
@@ -1,16 +1,16 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import NavBar from "./components/navbar";
+import NavBar from "../components/navbar";
 
-export default function Home() {
+export default function AboutUs() {
   
 const router = useRouter();
 
   return (
   <div className="flex flex-col min-h-screen"> 
     <NavBar/>
-    <h1>HOME</h1>
+    <h1>ABOUT US</h1>
   </div>
   );
 }
--- app/services/page.js
+++ app/services/page.js
@@ -1,16 +1,16 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import NavBar from "./components/navbar";
+import NavBar from "../components/navbar";
 
-export default function Home() {
+export default function Services() {
   
 const router = useRouter();
 
   return (
   <div className="flex flex-col min-h-screen"> 
     <NavBar/>
-    <h1>HOME</h1>
+    <h1>SERVICES</h1>
   </div>
   );
 }
